808a61e7dac8e64b0bb7e5b4eabf1e6898aa2ae7
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2025-05-22 00:30:14 +0630

    uploaded files

---
+++ MiniWallet.Domain/Features/TransferServices/TransferRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWallet.Domain.Features.TransferServices
+{
+    public class TransferRequestModel
+    {
+        public string? FromMobileNumber { get; set; }
+        public string? ToMobileNumber { get; set; }
+        public decimal? amount { get; set; }
+
+    }
+}
